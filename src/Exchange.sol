0a066b38ca5e84427e1017dfcc30c9dee3099285
Kiran Cherukuri <kirancherukuri@Kirans-MacBook-Pro.local>
2022-05-07 17:23:50 -0400

    added initial attempt at hyperobject template compatible w exchange contract

--- src/Exchange.sol
+++ src/Exchange.sol
@@ -4,7 +4,7 @@
 /// Is honestly being crowded by these grown thoughts
 
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.11;
+pragma solidity ^0.8.11; 
 
 import "./interfaces/IBondingCurve.sol";
 import "./interfaces/IHyperobject.sol";
