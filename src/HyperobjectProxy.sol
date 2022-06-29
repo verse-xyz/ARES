665a32212132b8579f2f73149e0c5fc15370b905
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-06-29 00:06:51 -0400

    add ownable and metadata rederer interfaces

--- src/HyperobjectProxy.sol
+++ src/HyperobjectProxy.sol
@@ -9,3 +9,6 @@ contract HyperobjectProxy is ERC1967Proxy {
     ERC1967Proxy(_logic, _data)
   {}
 }
+
+// ERC1967Upgrade defines the getter and setter functions for EIP1967 slots
+// ERC1967Proxy is the actual proxy contract implementation that uses those functions
---
+++ src/interfaces/IOwnable.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.11;
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * This ownership interface matches OpenZeppelin's ownable interface.
+ *
+ */
+interface IOwnable {
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() external view returns (address);
+}
