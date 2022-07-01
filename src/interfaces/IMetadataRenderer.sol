c345ae373c67853ff56f0891a9beeed4d2628b08
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-07-01 19:55:27 -0400

    remove contract uri for now

--- src/interfaces/IMetadataRenderer.sol
+++ src/interfaces/IMetadataRenderer.sol
@@ -3,6 +3,5 @@ pragma solidity ^0.8.11;
 
 interface IMetadataRenderer {
   function tokenURI(uint256) external view returns (string memory);
-  function contractURI() external view returns (string memory);
   function initializeWithData(bytes memory initData) external;
 }
