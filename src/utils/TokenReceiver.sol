7ea8b65f87ba1367db798a396589a5c5b61509c8
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-09-17 19:43:47 -0400

    ERC721 Infra

---
+++ src/utils/TokenReceiver.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+/// @notice Modified from OpenZeppelin Contracts v4.7.3 (token/ERC721/utils/ERC721Holder.sol)
+abstract contract ERC721TokenReceiver {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external virtual returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
