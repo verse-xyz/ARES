712dbb427429616929815b47c02d68ea48ce4c85
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-09-20 10:38:44 -0400

    scope out token functions

--- src/token/Token.sol
+++ src/token/Token.sol
@@ -7,6 +7,7 @@ import { ERC721 } from "../token/ERC721.sol";
 import { TokenStorage } from "./storage/TokenStorage.sol";
 import { IToken } from "./interfaces/IToken.sol";
 import { IImage } from "../image/interfaces/IImage.sol";
+import { Image } from "../image/Image.sol";
 
 
 contract Token is IToken, ERC721, UUPS, ReentrancyGuard, TokenStorage {
@@ -25,9 +26,10 @@ contract Token is IToken, ERC721, UUPS, ReentrancyGuard, TokenStorage {
     address _image
   ) external initializer {
     __ReentrancyGuard_init();
-    (string memory _name, string memory _symbol, string memory _initImageURI, string memory _initInteractionsURI) = abi.decode(_initStrings, (string, string, string, string));
+    (string memory _name, string memory _symbol, string memory _initImageURI) = abi.decode(_initStrings, (string, string, string));
     __ERC721_init(_name, _symbol);
     config.image = _image;
+
   }
 
   /*//////////////////////////////////////////////////////////////
@@ -37,5 +39,11 @@ contract Token is IToken, ERC721, UUPS, ReentrancyGuard, TokenStorage {
   // knit
   // mirror
   // burn
-  // creator mint
+  // creator knit
+  // creator mirror
+
+  // we want to give the creator "equity" 
+  // that is, they have the right to mint a certain amount of tokens at no cost
+  // show the creator how many "free mints" they have left?
+
 }
