d2f05ccc816a2fd81dd38ed9e8fe53362db54f8a
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-09-27 16:09:20 -0400

    remove factory types separate file

--- src/factory/interfaces/IFactory.sol
+++ src/factory/interfaces/IFactory.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.11;
 
 import { IOwnable } from "../../interfaces/IOwnable.sol";
-import { FactoryTypes } from "../types/FactoryTypes.sol";
 
-interface IFactory is FactoryTypes {
+interface IFactory {
   /*//////////////////////////////////////////////////////////////
                             ERRORS
   //////////////////////////////////////////////////////////////*/
@@ -20,6 +19,22 @@ interface IFactory is FactoryTypes {
   /// @param token The ERC-721 token address
   /// @param image The image rendering address
   event HyperimageDeployed(address token, address image);
+
+  /*//////////////////////////////////////////////////////////////
+                            STRUCTS
+  //////////////////////////////////////////////////////////////*/
+
+  /// @notice The hyperimage token parameters
+  /// @param initStrings The encoded token name and initializing image URI
+  /// @param targetPrice The target price for a token if sold on pace, scaled by 1e18
+  /// @param priceDecreasePercent Percent price decrease per unit of time, scaled by 1e18
+  /// @param perTimeUnit The number of tokens to target selling in 1 full unit of time, scaled by 1e18
+  struct TokenParams {
+      bytes initStrings;
+      int256 targetPrice;
+      int256 priceDecayPercent;
+      int256 perTimeUnit;
+  }
 
   /*//////////////////////////////////////////////////////////////
                             FUNCTIONS
--- src/test/Utils/HyperimageTest.sol
+++ src/test/Utils/HyperimageTest.sol
@@ -5,6 +5,7 @@ import { Test } from "forge-std/Test.sol";
 
 import { IFactory, Factory } from "../../factory/Factory.sol";
 import { IToken, Token } from "../../token/Token.sol";
+import { IUniversalImageStorage, UniversalImageStorage } from "../../image/storage/UniversalImageStorage.sol";
 import { IImage, Image } from "../../image/Image.sol";
 import { IARES, ARES } from "../../market/ARES.sol";
 
@@ -15,15 +16,56 @@ contract HyperimageTest is Test {
 
   Factory internal factory;
 
+  address internal factoryImpl;
   address internal tokenImpl;
   address internal imageImpl;
+  address internal universalImageStorage;
 
   address internal creator;
+  address internal verse;
 
   function setUp() public virtual {
     creator = vm.addr(0xA11CE);
-    vm.label(creator, "CREATOR");
+    verse = vm.addr(0xB0B);
 
-    
+    vm.label(creator, "CREATOR");
+    vm.label(verse, "VERSE");
+
+    factoryImpl = address(new Factory());
+    factory = Factory(factoryImpl);
+
+    tokenImpl = address(new Token(address(factory)));
+    universalImageStorage = address(new UniversalImageStorage(address(factory)));
+    imageImpl = address(new Image(address(factory), address(universalImageStorage)));
+
+    vm.prank(verse);
+    factory.initialize(tokenImpl, imageImpl, universalImageStorage);
+
+  }
+
+  /*//////////////////////////////////////////////////////////////
+                          HYPERIMAGE CUSTOMIZATION UTILS
+  //////////////////////////////////////////////////////////////*/
+
+  IFactory.TokenParams internal tokenParams;
+
+  function setMockTokenParams() internal virtual {
+
+  }
+
+  function setTokenParams(
+    string memory _name,
+    string memory _initImageURI,
+    int256 _targetPrice,
+    int256 _priceDecayPercent,
+    int256 _perTimeUnit
+  ) internal virtual {
+    bytes memory initStrings = abi.encode(_name, _initImageURI);
+    tokenParams = IFactory.TokenParams({
+      initStrings: initStrings,
+      targetPrice: _targetPrice,
+      priceDecayPercent: _priceDecayPercent,
+      perTimeUnit: _perTimeUnit
+    }); 
   }
 }
