2746e2515c05a0088d308d507ca22d66a679f22a
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-09-27 14:24:09 -0400

    going to refactor factory contract so that we don't have immutables

--- src/test/Utils/HyperimageTest.sol
+++ src/test/Utils/HyperimageTest.sol
@@ -1,4 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import { Test } from "forge-std/Test.sol";
+import { Test } from "forge-std/Test.sol"; 
+
+import { IFactory, Factory } from "../../factory/Factory.sol";
+import { IToken, Token } from "../../token/Token.sol";
+import { IImage, Image } from "../../image/Image.sol";
+import { IARES, ARES } from "../../market/ARES.sol";
+
+contract HyperimageTest is Test {
+  /*//////////////////////////////////////////////////////////////
+                            BASE SETUP
+  //////////////////////////////////////////////////////////////*/
+
+  Factory internal factory;
+
+  address internal tokenImpl;
+  address internal imageImpl;
+
+  address internal creator;
+
+  function setUp() public virtual {
+    creator = vm.addr(0xA11CE);
+    vm.label(creator, "CREATOR");
+
+    
+  }
+}
