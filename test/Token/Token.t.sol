c9fe02a7c9c0362ba94875e4de02eaba089d2dcc
Kiran Cherukuri <kiranecherukuri@gmail.com>
2022-09-28 14:17:16 -0400

    refactor

--- test/Token/Token.t.sol
+++ test/Token/Token.t.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.11;
 
 import { HyperimageTest } from "../Utils/HyperimageTest.sol";
 
-import { IFactory, Factory } from "../../factory/Factory.sol";
-import { IToken, Token } from "../../token/Token.sol";
-import { IImage, Image } from "../../image/Image.sol";
-import { IARES, ARES } from "../../market/ARES.sol";
+import { IFactory, Factory } from "../../src/factory/Factory.sol";
+import { IToken, Token } from "../../src/token/Token.sol";
+import { IImage, Image } from "../../src/image/Image.sol";
+import { IARES, ARES } from "../../src/market/ARES.sol";
 
-import { TokenTypes } from "../../token/types/TokenTypes.sol";
+import { TokenTypes } from "../../src/token/types/TokenTypes.sol";
 
 contract TokenTest is HyperimageTest, TokenTypes {
   function setUp() public virtual override {
--- test/Utils/HyperimageTest.sol
+++ test/Utils/HyperimageTest.sol
@@ -3,17 +3,17 @@ pragma solidity ^0.8.11;
 
 import { Test } from "forge-std/Test.sol"; 
 
-import { IFactory, Factory } from "../../factory/Factory.sol";
-import { IToken, Token } from "../../token/Token.sol";
-import { IUniversalImageStorage, UniversalImageStorage } from "../../image/storage/UniversalImageStorage.sol";
-import { IImage, Image } from "../../image/Image.sol";
-import { IARES, ARES } from "../../market/ARES.sol";
+import { IFactory, Factory } from "../../src/factory/Factory.sol";
+import { IToken, Token } from "../../src/token/Token.sol";
+import { IUniversalImageStorage, UniversalImageStorage } from "../../src/image/storage/UniversalImageStorage.sol";
+import { IImage, Image } from "../../src/image/Image.sol";
+import { IARES, ARES } from "../../src/market/ARES.sol";
 
 contract HyperimageTest is Test {
   /*//////////////////////////////////////////////////////////////
                             BASE SETUP
   //////////////////////////////////////////////////////////////*/
-
+ 
   Factory internal factory;
 
   address internal factoryImpl;
